b3f4fb2ee7b9a13cf747aa48f528e28128a7169c
Robert Haji <roberthadji66@gmail.com>
2025-05-23 17:49:38 +0300

    initializers-and-properties

--- coffee.py
+++ coffee.py
@@ -1,2 +1,24 @@
 class Coffee:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+    @property
+    # Use property Getter to return the value of the private attribute _name
+    def name(self):
+        return self._name 
+    # Use the property setter
+
+    def name (self, value):
+        if isinstance (value, str) and len(value) >= 3:
+            self.name = value
+        else:
+            raise ValueError ("Name must be the value of string atleast above 3")
+        
+#  Debugging with print statements
+# coffee = Coffee("Capuccinno")  
+
+# print(coffee.name)
+
+# coffee = Coffee("C")
+    
+   
--- customer.py
+++ customer.py
@@ -1,2 +1,23 @@
 class Customer:
-    pass
+    def __init__(self, name):
+      self.name = name
+
+# Use property Getter to return the value of the private attribute _name
+@property
+
+def name (self):
+   return self._name
+
+# Use property setter 
+
+def name (self, value):
+   if isinstance (value, str) and 1 <= len(value) <= 15:
+      self._name = value
+   else:
+      raise ValueError("Name must be the value of string between 1 and 15 characters") 
+   
+#    Debugging and testing
+# customer = Customer("Robert Haji")
+
+# print(customer.name)
+
